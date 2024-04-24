66c2a67c5573211ff2960e3b748c8ce86f92e4bc
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-04-24 12:30:44 +0530

    connect db

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const app = express()
+const pg= require('pg')
+const ejs = require('ejs')
+app.use(bodyparser.urlencoded({extended:true}))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+var data
+const db = new pg.Client({
+    user:'postgres',
+    host:'localhost',
+    database:'world',
+    password:'vaibhav',
+    port:5432
+});
+
+db.connect();
+
+db.query("SELECT * FROM countries", (err,res)=>
+{
+    if(err){
+        console.log('error found , no data recieved')
+    }
+    else{
+        data=res.rows;
+    }
+
+    db.end();
+});
+
+
+
+app.get('/',(req,res)=>
+{
+    res.render('map')
+})
+app.listen(3000)
