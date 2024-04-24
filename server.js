cb1295e7e1f4208227f0ef493abb9d54f7d3238c
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-04-25 01:01:49 +0530

    fetching data from databse

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express()
 const pg= require('pg')
 const ejs = require('ejs')
 var marked_country
+var row
 app.use(bodyparser.urlencoded({extended:true}))
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-var data
 const db = new pg.Client({
     user:'postgres',
     host:'localhost',
@@ -19,10 +19,13 @@ const db = new pg.Client({
 db.connect();
 app.post('/', async(req,res)=>
 {
-    const marked_country = await req.body.country;
-
+    marked_country = await req.body.country;
+    marked_country=marked_country.charAt(0).toUpperCase()+ marked_country.slice(1)
     Promise.resolve(marked_country)
       .then((country) => {
+
+        // inserting into database-->
+
         const insertQuery =("INSERT INTO visited (country) VALUES ($1)")
         db.query(insertQuery,[country],(err,res)=>
         {
@@ -30,18 +33,38 @@ app.post('/', async(req,res)=>
          {
            console.log(err)
          }
+         else{
+            console.log(res.rows)
+         }
         })
+    // fetching rows from database-->
+    
+        db.query('SELECT * FROM visited',(err,res)=>
+      {
+        if(err)
+        {
+           console.log(err)
+        }else
+        {
+        row=res.rows
+        }
+      })
 
         res.redirect('/')
-    })
-      .catch((err) => {
+    }).catch((err) => {
         console.error('Error:', err);
         res.status(500).send('Error occurred');
       });
 })
 
+
+
 app.get('/',(req,res)=>
 {
-    res.render('map')
+    if(row){
+      res.render('map')
+    }else{
+        res.render('map')
+    }
 })
 app.listen(3000)
